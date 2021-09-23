a085b2e5194a19e481dd141aa058779977562e6f
Jon Ormond <tracsman@hotmail.com>
2021-09-23 14:53:10 -0700

    Display model is code complete

--- TextGame.py
+++ TextGame.py
@@ -1,9 +1,10 @@
 import random
 import json
 
-# Color class for adding color to the print output
-# Idea from https://appdividend.com/2021/06/14/how-to-print-bold-python-text
-# Define the class and add constant values for each color/effect
+# Color class for adding color to print output
+# From https://appdividend.com/2021/06/14/how-to-print-bold-python-text
+# (c) Krunal Lathiya, June 14, 2021
+# Defines the class color and adds constant values for each color/effect
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -29,6 +30,7 @@ def main():
     # Load map file
     print("\nLoading Map File......", end="")
     with open(map_file, "r") as read_file:
+        global map
         map = json.load(read_file)
         print("Done")
 
@@ -50,40 +52,48 @@ def main():
     hero_list = ["Bubba", "Fighter"]  # TODO: Remove before production
     hero_name = hero_list[0]
     hero_class = hero_list[1]
-    hero_inventory = ["Empty"]
-    hero_weapon = "Fists"
-    hero_life = 100
-    current_room_index = "0" # TODO: reset to 0 before production
+    # hero_inventory = ["Empty"]
+    hero_inventory = ["1","2","3"]    # TODO: reset to "Empty" before production
+    #hero_weapon = "Fists"
+    hero_weapon = "9"                # TODO: reset to "Fists" before production
+    # hero_life = 100
+    hero_life = 25                   # TODO: reset to "100" before production
+    current_room_index = "0"          # TODO: reset to 0 before production
     
     # Display intro message from the King
     print(SECTION_BREAK)
-    print('Hello {} brave {}!!\n'.format(hero_name, hero_class))
+    print('Greetings {}, brave {}!!\n'.format(hero_name, hero_class))
     print('Thank you for taking on this foolhardy.... er... I mean... uh')
     print('valiant! yes, valiant quest! My kingdom has been overrun by evil')
-    print('doers and my treasury plundered. Please get my precious gemstones')
-    print('back. There are six in all. When you have collected them please')
-    print('return to me here in the castle.\n')
-    print('Best of luck!\n')
+    print('doers and my treasury plundered. Please retreive my precious')
+    print('gemstones. There are six in all. When you have collected them,')
+    print('please return to me here in the castle.\n')
+    print('Best of luck!')
     print(SECTION_BREAK)
             
     # Start main game loop
+    # Although set up as an infinite loop, there are exit points within
+    # the main loop based on completion of the game or at the request
+    # of the user.
     while True:
-        # Display the standard output screen information:
+        # The main purpose of this loop is to display the standard
+        # output screen information and accept and handoff the user
+        # command. The display output is aligned thusly:
         # 
         # Current location description
-        # Any items in the room
-        # Any monsters in the room
+        # (optional) Any items in the room
+        # (optional) Any monsters in the room
         #
         # The hero's inventory
         # The hero's weapon
         # The hero's health
         #
-        # The possible directions and whats in that direction
+        # The possible directions and what is in that direction
         # 
         # A reminder of the verbs
         # An input prompt for a command
         #
-        #
+
         # Show the description of the current room from the map dictionary
         print(map[current_room_index]['Description'])
 
@@ -98,20 +108,41 @@ def main():
         # Show any monsters
         if 'Monster' in map[current_room_index]:
             print('Oh no! There is a big scary ' + color.RED + monsters[map[current_room_index]['Monster']]["Name"] + color.END + ' in here with you!!')
+
         # Show the hero's current inventory list
-        print('\nYour Inventory:', hero_inventory)
+        if "Empty" not in hero_inventory:
+            inventory_items = '\nYour Inventory: '
+            for item in hero_inventory:
+                inventory_items += color.GREEN + items[item]['Name'] + color.END + ', '
+            inventory_items = inventory_items[:-2]
+            print(inventory_items)
+        else:
+            print('\nYour Inventory: Empty')
+
         # Show the hero's current equipped weapon
-        print('Your Weapon:', hero_weapon)
+        if hero_weapon != "Fists":
+            print('Your Weapon: ' + color.GREEN + items[hero_weapon]["Name"] + color.END)
+        else:
+            print('Your Weapon: Fists')
+            
         # Show the hero's remaining life
-        print('Your health: {} out of 100\n'.format(hero_life))
+        if hero_life > 80:
+            life_color = color.GREEN
+        elif hero_life > 30:
+            life_color = color.YELLOW
+        else:
+            life_color = color.RED + color.BOLD
+        print('Your health: ' + life_color + str(hero_life) + color.END +' out of 100\n')
+        
         # Show the possible directions available from this location
         # Start with a beging of the line in a variable and add each valid direction to the string
         directions = "Possible Directions: you see "
-        # Check each the dictionary for the current room index, and see if the cardinal direction exists
+        # Check each the map dictionary for the current room index and see if the cardinal direction exists
         if 'North' in map[current_room_index]:
             # If this direction exists, append this direction, nicely formated with an ending comma to the directions string
-            # Note: that the map object is being called twice in the format statement, once to pull the index number from the
-            #       requested direction of the current room to be used as the index to get the name of the intended room.
+            # Note: the map dictionary is being called twice (nested) in the format statement, once to pull the index number
+            #       from the requested direction of the current room to be used as the index to get the name of the intended
+            #       room.
             directions +='a {} to the North, '.format(map[map[current_room_index]['North']]['Name'])
         if 'East' in map[current_room_index]:
             directions +='a {} to the East, '.format(map[map[current_room_index]['East']]['Name'])
@@ -120,7 +151,8 @@ def main():
         if 'West' in map[current_room_index]:
             directions +='a {} to the West, '.format(map[map[current_room_index]['West']]['Name'])
         # The last direction appended will have a trailing comma and a space, so we need to trim those two characters off
-        directions = directions[:-2]
+        # and add a period
+        directions = directions[:-2] + "."
         # Print the available directions for this location
         print(directions, '\n\n')
         # Print a reminder of the available verbs for the user to use
@@ -128,7 +160,8 @@ def main():
         # Get the users command
         user_input = input('Please enter a command (verb noun): ')
         # Check if the user just wants to end the game
-        if user_input.lower() in ['quit','exit','bye','goodbye','later']:
+        # Change the input to lower case, and split off anything after the verb if the user added a noun
+        if user_input.lower().split(' ')[0] in ['quit','exit','bye','goodbye','later']:
             # Double check as this eliminates any progress made in the game
             temp = input('You have asked to quit the game! Are you sure? Y/n ') or 'y'
             # Eval the users second answer
@@ -172,8 +205,10 @@ def main():
             print(color.CYAN + color.BOLD + 'Invalid input' + color.END + ': the verb used was not recognized, please try again.\n')
 
 def move(room_index, direction):
-    print('move func{}'.format(direction))
-    new_room_index = 0
+    print('move func({})'.format(direction))
+    print(map["0"]['Description'])
+    #print(map[room_index])
+    new_room_index = "0"
     return new_room_index
     
 def look():
